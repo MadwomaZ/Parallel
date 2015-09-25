efdb48526debd1082e4cf3ef16a761d5a5396b72
Evgeniya [MadWomaZ] Kharlamova <madwomaz@evgeny.by>
2015-09-26 01:29:50 +0300

    Make file string_tr with threads - it is work

--- strings_tr.cpp
+++ strings_tr.cpp
@@ -5,49 +5,57 @@
 #include <fstream>
 #include <thread>
 #include <string>
+#include <vector>
  
-//static const int num_threads = 10;
- 
-     //void call_from_thread(int tid) 
-//	{
-  //      std::cout << "Launched by thread " << tid << std::endl;
-    // }
- 
-     void empty_string(int tid) 
-	{
-	int num = 0;
-	std::string str;
-	std::ifstream ios("mass_tr.cpp");
-	while(ios.good())
-	{
-		num++;
-		std::getline(ios, str);
-		if (str.length() == 0)
-		{
-			std::cout << "In thread of" << tid <<  " empty string " << num << std::endl;
-		}
-	}
-	}
+void readFile(std::vector<std::string> *strings)
+{
+    std::string str;
+    std::ifstream ios("mass_tr.cpp", std::ios::in);
+    while (getline(ios, str))
+    {
+        strings->push_back(str);
+//        std::cout << str << '\n';
+    }
+}
+
+void empty_string(std::vector<std::string> *strings, int tid, int numtr)
+{
+    for(int i = 0; i < strings->size(); i++)
+        if(i % numtr == tid)
+            if (strings->at(i).length() == 0)
+                std::cout << "In thread of " << tid <<  " empty string " << i + 1 << std::endl;
+//    int num = 0;
+//    std::string str;
+//    std::ifstream ios("mass_tr.cpp");
+//    while(ios.good())
+//    {
+//        num++;
+//        std::getline(ios, str);
+//        if (str.length() == 0)
+//        {
+//            std::cout << "In thread of" << tid <<  " empty string " << num << std::endl;
+//        }
+//    }
+}
 	
-	int main()
-	{
-	int numtr = 0;
-	std::cout << "Enter the number of threads: ";
-	std::cin >> numtr;
+int main()
+{
+    int numtr = 0;
+    std::cout << "Enter the number of threads: ";
+    std::cin >> numtr;
+    std::vector<std::string> strings;
 
-         std::thread t[numtr];
- 
-        for (int i = 0; i < numtr; ++i) 
-	{
-             t[i] = std::thread(empty_string, i);
-         }
+    std::thread t[numtr];
 
-        std::cout << "Launched from the main\n";
+    readFile(&strings);
+
+    for (int i = 0; i < numtr; ++i)
+        t[i] = std::thread(empty_string, &strings, i, numtr);
+
+    std::cout << "Launched from the main\n";
+
+    for (int i = 0; i < numtr; ++i)
+        t[i].join();
  
-        for (int i = 0; i < numtr; ++i) 
-	{
-             t[i].join();
-         }
- 
-         return 0;
-     }
+    return 0;
+}
